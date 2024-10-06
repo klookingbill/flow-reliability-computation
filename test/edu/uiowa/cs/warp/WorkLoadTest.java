fa24c697af1f0b76004d47b4d5a410c7ac8ca515
klookingbill <klookingbill@uiowa.edu>
2024-10-06 15:10:55 -0500

    WorkLoadTest v.1

---
+++ test/edu/uiowa/cs/warp/WorkLoadTest.java
@@ -0,0 +1,15 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WorkLoadTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		assertTrue();
+	}
+
+}
